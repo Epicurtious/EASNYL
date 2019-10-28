932e7f7ff4ffc8990e5bb782955346c1234f5c07
Curtis Goodwin <curtisgoodwin117@yahoo.com>
2019-10-28 17:43:37 -0400

    Version 1.1
    Put in while loop, adds time, sleeps so doesn't reach API quota

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@ import requests
 from pprint import pprint
 from collections import defaultdict
 from datetime import datetime
+import time
 
 #returns number of seconds in day today
 #for comparing time by integer than by string
@@ -38,66 +39,135 @@ def textOfList(arr):
         texts.append(html.text)
     return texts
 
-# gets authorization to use api
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
-client = gspread.authorize(creds)
+while(True):
+    # gets authorization to use api
+    scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+    client = gspread.authorize(creds)
 
-todate = datetime.today()
-today = todate.strftime("%b-%d-")
-today += str(todate.year-2000)
+    #gets todays date and puts it in the correct format: Sep-20-19
+    todate = datetime.today()
+    today = todate.strftime("%b-%d-")
+    today += str(todate.year-2000)
 
-spreadsheet = client.open("EASNYL") # spreadsheet base
-titles = spreadsheetTitles(spreadsheet) # titles of each worksheet
-linkSheet = spreadsheet.worksheet('Links') # spreadsheet with links and names
-linkSheetContents = linkSheet.get_all_values() # contents of linkSheet
-linkSheetContents.pop(0) # takes heading out
-names = [row[0] for row in linkSheetContents] # names in the linkSheet
-makeUnmadeTitles(titles, names, spreadsheet) # creates unmade titles
-for row in linkSheetContents:
-    urlBase = 'https://finviz.com/' # base for hrefs
-    title = row[0] # title of worksheet
-    worksheet = spreadsheet.worksheet(title) # worksheet to use
-    link = row[1] # link in row
-    html = requests.get(link).content # html of link
-    soup = bs(html, 'lxml') # soup object of link
-    table = soup.find(id='screener-content') # table with all contents
-    newsURI = table.find('a', text='News')['href'] # URI to news section for scraping
-    newsURL = [] # list for two news links
-    newsURL.append(urlBase + newsURI) # url for news first 10
-    newsURL.append(urlBase + newsURI + "&r=11") # url for news second 10
-    header = table.find(valign='middle', align='center') # header of table, will be header of sheet
-    header = header.findAll('td') # td tags of header, tags that have text
-    headerTexts = textOfList(header) # texts inside header
-    headerTexts.insert(0,'Date') # puts "Date" at the start
-    headerTexts.append('News Headline') # puts "News Headling" at the end
-    headerTexts.append('Hyperlink') # puts "Hyperlink" at the end
-    worksheetHeader = worksheet.row_values(1) # header row of worksheet
-    if(worksheetHeader != headerTexts): # checks if header is already there
-        worksheet.insert_row(headerTexts) # puts header into sheet
-    tickerNewsDict = defaultdict(list) # dictionary to get news articles
-    currentHyperlinks = worksheet.col_values(len(headerTexts)) # list of hyperlinks in spreadsheet already
-    for news in newsURL:
-        newsHtml = requests.get(news).content # html of news section
-        newsSoup = bs(newsHtml, 'lxml') # soup object of news
-        for unit, symbol in zip(newsSoup.find_all('table', class_="body-table-news"), newsSoup.find_all(class_="snapshot-table")):
-            ticker = symbol.a.text # ticker of news
-            dateNews = unit.tr # tr tag that had date and news
-            date = dateNews.td.text.split()[0] # date of news article, will be time if it differs by time
-            if(date == today):
-                info = dateNews.a # news info
-                hyperlink = info['href'] # link to news article
-                headline = info.text # headline of news article
-                if(hyperlink not in currentHyperlinks):
-                    tickerNewsDict[ticker] = [headline, hyperlink] # add to dictionary to later access
-    tickersToCheck = tickerNewsDict.keys() # tickers that had news articles
-    for row in table.find_all(True, {'class':['table-dark-row-cp', 'table-light-row-cp']}):
-        ticker = row.td.text
-        if ticker in tickersToCheck: # checks if this ticker has news
-            rowInsert = [] # list to put into google sheets
-            rowInsert.append(today) # put date at the start
-            for info in row.find_all('td'):
-                rowInsert.append(info.text) # adds info to list
-            for x in tickerNewsDict[ticker]:
-                rowInsert.append(x) # appends headline and hyperlink
-            worksheet.insert_row(rowInsert, 2) # puts in spreadsheet
+    # spreadsheet base
+    spreadsheet = client.open("EASNYL")
+    # titles of each worksheet
+    titles = spreadsheetTitles(spreadsheet)
+    # spreadsheet with links and names
+    linkSheet = spreadsheet.worksheet('Links')
+    # contents of linkSheet
+    linkSheetContents = linkSheet.get_all_values()
+    # takes heading out
+    linkSheetContents.pop(0)
+    # names in the linkSheet
+    names = [row[0] for row in linkSheetContents]
+    # creates unmade titles
+    makeUnmadeTitles(titles, names, spreadsheet)
+    for row in linkSheetContents:
+        # base for hrefs
+        urlBase = 'https://finviz.com/'
+        # title of worksheet
+        title = row[0]
+        # worksheet to use
+        worksheet = spreadsheet.worksheet(title)
+        # link in row
+        link = row[1]
+        # html of link
+        html = requests.get(link).content
+        # soup object of link
+        soup = bs(html, 'lxml')
+        # table with all contents
+        table = soup.find(id='screener-content')
+        # URI to news section for scraping
+        newsURI = table.find('a', text='News')['href']
+        # list for two news links
+        newsURL = []
+        # url for news first 10
+        newsURL.append(urlBase + newsURI)
+        # url for news second 10
+        newsURL.append(urlBase + newsURI + "&r=11")
+        # header of table, will be header of sheet
+        header = table.find(valign='middle', align='center')
+        # td tags of header, tags that have text
+        header = header.findAll('td')
+        # texts inside header
+        headerTexts = textOfList(header)
+        # puts "Date" at the start
+        headerTexts.insert(0,'Date')
+        # puts "Time" in header
+        headerTexts.insert(1,"Time")
+        # puts "News Headling" at the end
+        headerTexts.append('News Headline')
+        # puts "Hyperlink" at the end
+        headerTexts.append('Hyperlink')
+        # header row of worksheet
+        worksheetHeader = worksheet.row_values(1)
+        # checks if header is already there
+        if(worksheetHeader != headerTexts):
+            # puts header into sheet
+            worksheet.insert_row(headerTexts)
+        # dictionary to get news articles
+        tickerNewsDict = defaultdict(list)
+        # list of hyperlinks in spreadsheet already
+        currentHyperlinks = worksheet.col_values(len(headerTexts))
+        # goes through each block of news on the page
+        for news in newsURL:
+            # html of news section
+            newsHtml = requests.get(news).content
+            # soup object of news
+            newsSoup = bs(newsHtml, 'lxml')
+            # unit gets the news data, symbol gets the ticker
+            for unit, symbol in zip(newsSoup.find_all('table', class_="body-table-news"), newsSoup.find_all(class_="snapshot-table")):
+                # ticker of news
+                ticker = symbol.a.text
+                # tr tag that had date and news
+                dateNews = unit.tr
+                # date and time of news article
+                dateAndTime = dateNews.td.text.split()
+                # separate the date
+                date = dateAndTime[0]
+                # separate the time
+                timeOf = dateAndTime[1]
+                # checks if the article is from today
+                if(date == today):
+                    # news <a> tag, has information about article
+                    info = dateNews.a
+                    # link to news article
+                    hyperlink = info['href']
+                    # headline of news article
+                    headline = info.text
+                    # checks if the hyperlink is already in the google sheet
+                    if(hyperlink not in currentHyperlinks):
+                        # add to dictionary to later access
+                        tickerNewsDict[ticker] = [timeOf, headline, hyperlink]
+        # tickers that had news articles
+        tickersToCheck = tickerNewsDict.keys()
+        # goes through each stock on the page
+        for row in table.find_all(True, {'class':['table-dark-row-cp', 'table-light-row-cp']}):
+            # ticker symbol of current row
+            ticker = row.td.text
+            # checks if this ticker has news
+            if ticker in tickersToCheck:
+                # list to put into google sheets
+                rowInsert = []
+                # put date at the start
+                rowInsert.append(today)
+                # time of article
+                timeOf = tickerNewsDict[ticker][0]
+                # put time after date
+                rowInsert.append(timeOf)
+                # goes through all the data from each stock
+                for info in row.find_all('td'):
+                    # adds info to list
+                    rowInsert.append(info.text)
+                # goes through each of the tickers info
+                for x in range(1,3):
+                    # info to append
+                    unit = tickerNewsDict[ticker][x]
+                    # appends headline and hyperlink
+                    rowInsert.append(unit)
+                # puts in spreadsheet
+                worksheet.insert_row(rowInsert, 2)
+    # waits 15 secs so to not call the API too many times
+    time.sleep(15)
